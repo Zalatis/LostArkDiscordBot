1ba8b628823986f2b663e341cef90cfc84f6406d
Zalati (Cédric Lecullié) <y4su.ced@gmail.com>
2022-06-10 02:24:32 +0200

    Update forum_news.py

--- bot/cogs/forum_news.py
+++ bot/cogs/forum_news.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext import commands,tasks
-import urllib
-from urllib.request import Request, urlopen
-from bs4 import BeautifulSoup as soup
+import re
+import httpx
 
 class LostArkForumNews(commands.Cog):
     def __init__(self, bot:commands.Bot):
@@ -13,7 +12,7 @@ class LostArkForumNews(commands.Cog):
         self.color = 1179867
         self.update_news.start()
         self.last_news_title = None
-        self.last_news_description = None
+        self.last_news_post_content = None
         self.last_news_message_id = None
         self.msg = None
 
@@ -23,47 +22,66 @@ class LostArkForumNews(commands.Cog):
     def get_news_info(self):
         # Gets source of Latest official news from Forum.
         try:
-            request = Request("https://forums.playlostark.com/c/official-news/annonces-officielles-et-evenements/18", headers={'User-Agent': 'Mozilla/5.0'})
-            source = urlopen(request).read()
-        except:
-            raise Exception("Couldn't connect to " + self.name + "' website.")
+            url = (
+                "https://forums.playlostark.com/c/official-news/annonces-officielles-et-evenements/18/l/latest.json?ascending=false"
+                )
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
+            }
+            response = httpx.get(url, headers=headers)
+            apiResponse = response.json()
+            base = apiResponse["topic_list"]["topics"]
 
-        try:
-            news_span = soup(source, "html.parser").findAll("span",{"class":"link-top-line"})
-        except:
-            raise Exception("Error retrieving news_span")
+            self.api = []
+            for each in base:
+                post_id = each["id"]
+                clean_url = f"https://forums.playlostark.com/t/{post_id}.json"
+                response = httpx.get(clean_url, headers=headers)
+                responseJSON = response.json()
 
-        # Gets Lost Ark news url.
-        try:
-            self.news["url"] = news_span[0].a["href"]
-            if self.news["url"] is None:
-                raise Exception("Could not find " + self.name + " url.")
-        except:
-            raise Exception("Error retrieving " + self.name + " url. 1")
+                title = responseJSON["title"]
+                created_at = responseJSON["created_at"]
+                # grab last message in post
+                post_content = responseJSON["post_stream"]["posts"][-1]["cooked"]
+                # remove html tags from post content json string
+                post_content = re.sub(r"<.*?>", "", post_content)
+                # remove new lines from post content
+                post_content = re.sub(r"\n", " ", post_content)
+                # remove extra spaces from post content
+                post_content = re.sub(r"\s{2,}", " ", post_content)
+                # last message in post content
 
-        # Gets Lost Ark news title.
-        try:
-            news_title = soup(source, "html.parser").find("a",{"class":"title raw-link raw-topic-link"})
-            self.news["title"] = news_title.find(text=True)
-            if self.news["title"] is None:
-                raise Exception("Could not find " + self.name + " title.")
-        except:
-            raise Exception("Error retrieving " + self.name + " title.")
+                # check if post is pinned
+                pinned = responseJSON["pinned"]
+                staff = responseJSON["post_stream"]["posts"][0]["staff"]
 
-        # Gets Lost Ark news description
-        try:
-            request = Request(self.news["url"], headers={'User-Agent': 'Mozilla/5.0'})
-            source = urlopen(request).read()
-            forum_post = soup(source, "html.parser").find_all("div",{"class":"post"})[-1].getText()
-            post_text = forum_post.replace("\n", " ")
-            self.news["desc"] = post_text
-            if self.news["desc"] is None:
-                raise Exception("Could not find " + self.name + " description.")
-            if len(self.news["desc"]) > 400:
-                self.news["desc"] = self.news["desc"][:400] + "..."
-        except:
-            raise Exception("Error retrieving " + self.name + " description.")
-        return self
+                # author of post
+                author = responseJSON["post_stream"]["posts"][0]["username"]
+
+                # url to post
+                slug = responseJSON["slug"]
+                url = f"https://forums.playlostark.com/t/{slug}/{post_id}"
+
+                if not pinned and staff:
+                    self.api.append(
+                        {
+                            "title": title,
+                            "created_at": created_at,
+                            "post_content": post_content,
+                            "author": author,
+                            "url": url
+                        }
+                    )
+
+            self.title = self.api[0]["title"]
+            self.created_at = self.api[0]["created_at"]
+            self.post_content = self.api[0]["post_content"]
+            self.url = self.api[0]["url"]
+            self.author = self.api[0]["author"]
+
+            return self
+        except Exception as e:
+            print("Error while trying to get_news_info: ", e)
 
     # Updates Lost Ark news every 5 minutes.
     @tasks.loop(minutes=5)
@@ -71,9 +89,6 @@ class LostArkForumNews(commands.Cog):
         await self.bot.wait_until_ready()
         self.get_news_info()
         self.news_channel = await self.bot.fetch_channel(int(self.bot.NEWS_CHANNEL_ID))
-        self.title = self.news["title"].strip()
-        self.url = self.news["url"].strip()
-        self.desc = self.news["desc"].strip()
         try:
             with open ('./bot/cogs/txt_files/latest-news.txt', 'r', encoding='utf8') as f:
                 lines = f.readlines()
@@ -82,7 +97,7 @@ class LostArkForumNews(commands.Cog):
                     if line.startswith("Latest Title : "):
                         self.last_news_title = line.split("Latest Title : ")[1].strip()
                     elif line.startswith("Latest Description : "):
-                        self.last_news_description = None
+                        self.last_news_post_content = None
                     elif line.startswith("Message ID : "):
                         self.last_news_message_id = None
                     else:
@@ -99,13 +114,13 @@ class LostArkForumNews(commands.Cog):
                         if line.startswith("Latest Title : "):
                             self.last_news_title = line.split("Latest Title : ")[1].strip()
                         elif line.startswith("Latest Description : "):
-                            self.last_news_description = line.split("Latest Description : ")[1].strip()
+                            self.last_news_post_content = line.split("Latest Description : ")[1].strip()
                         elif line.startswith("Message ID : "):
                             self.last_news_message_id = line.split("Message ID : ")[1].strip()
                         else:
                             f.truncate()
                             self.last_news_title = None
-                            self.last_news_description = None
+                            self.last_news_post_content = None
                             self.last_news_message_id = None
                     f.close()
             except:
@@ -113,40 +128,39 @@ class LostArkForumNews(commands.Cog):
             if self.last_news_title != None:
                 if self.last_news_title != self.title:
                     # print("Le titre a changé")
-                    embed = discord.Embed(title=self.title, url=self.url, description=self.desc, color=self.color)
+                    embed = discord.Embed(title=self.title, url=self.url, description=self.post_content, color=self.color)
                     msg = await self.news_channel.send(embed=embed)
                     self.last_news_message_id = msg.id
                     try:
                         with open ('./bot/cogs/txt_files/latest-forum-news.txt', 'w+', encoding='utf8') as f:
                             f.seek(0)
                             f.truncate()
-                            f.write("Latest Title : " + self.title + "\nLatest Description : " + self.desc + "\nMessage ID : " + str(self.last_news_message_id))
+                            f.write("Latest Title : " + self.title + "\nLatest Description : " + self.post_content + "\nMessage ID : " + str(self.last_news_message_id))
                             f.close()
                     except:
                         raise Exception("Impossible d'ouvrir latest-forum-news.txt")
-                elif self.last_news_title == self.title and self.last_news_description != self.desc:
-                    # print("La description a changé")
+                elif self.last_news_title == self.title and self.last_news_post_content != self.post_content:
                     try:
                         with open ('./bot/cogs/txt_files/latest-forum-news.txt', 'w+', encoding='utf8') as f:
                             f.seek(0)
                             f.truncate()
-                            f.write("Latest Title : " + self.title + "\nLatest Description : " + self.desc + "\nMessage ID : " + str(self.last_news_message_id))
+                            f.write("Latest Title : " + self.title + "\nLatest Description : " + self.post_content + "\nMessage ID : " + str(self.last_news_message_id))
                             f.close()
                     except:
                         raise Exception("Impossible d'ouvrir latest-forum-news.txt")
                     self.msg = await self.news_channel.fetch_message(int(self.last_news_message_id))
-                    embed = discord.Embed(title=self.title, url=self.url, description=self.desc, color=self.color)
+                    embed = discord.Embed(title=self.title, url=self.url, description=self.post_content, color=self.color)
                     await self.msg.edit(embed = embed)
             else:
                 # print("Premier message")
-                embed = discord.Embed(title=self.title, url=self.url, description=self.desc, color=self.color)
+                embed = discord.Embed(title=self.title, url=self.url, description=self.post_content, color=self.color)
                 msg = await self.news_channel.send(embed=embed)
                 self.last_news_message_id = msg.id
                 try:
                     with open ('./bot/cogs/txt_files/latest-forum-news.txt', 'w+', encoding='utf8') as f:
                         f.seek(0)
                         f.truncate()
-                        f.write("Latest Title : " + self.title + "\nLatest Description : " + self.desc + "\nMessage ID : " + str(self.last_news_message_id))
+                        f.write("Latest Title : " + self.title + "\nLatest Description : " + self.post_content + "\nMessage ID : " + str(self.last_news_message_id))
                         f.close()
                 except:
                     raise Exception("Impossible d'ouvrir latest-forum-news.txt")
